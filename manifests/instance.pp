49d539945d5e3123f3c5998cd6f195414784c40c
Tino Mueller <tino.mueller@spreadgroup.com>
2024-04-02 14:28:21 +0200

    replace legacy fact with modern in instance.pp

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -202,7 +202,7 @@ define elasticsearch::instance (
     if('node.name' in $instance_config) {
       $instance_node_name = {}
     } else {
-      $instance_node_name = { 'node.name' => "${::hostname}-${name}" }
+      $instance_node_name = { 'node.name' => "${facts['networking']['hostname']}-${name}" }
     }
 
     # String or array for data dir(s)
